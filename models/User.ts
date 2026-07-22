8775cbc303150e957c78c86f244ae57e11207553
salima <sahisalima77@gmail.com>
2026-07-22 16:31:30 +0200

    crée schema user

---
+++ models/User.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
