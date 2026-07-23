252badc891d7ee4bf26082dd38f993ed8a37b21f
salima <sahisalima77@gmail.com>
2026-07-23 11:38:08 +0200

    travail sur la configure NextAuth credentials authentication

---
+++ lib/auth.ts
@@ -0,0 +1,78 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+import { connectDB } from "./mongodb";
+import User from "@/models/User";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Informations invalides");
+        }
+
+        await connectDB();
+
+        const user = await User.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          throw new Error("Email ou mot de passe incorrect");
+        }
+
+        const isMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!isMatch) {
+          throw new Error("Email ou mot de passe incorrect");
+        }
+
+        return {
+          id: user._id.toString(),
+          name: user.name,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
