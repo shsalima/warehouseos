134cc8b1b087f2193989dfed9133fd71d8f4d46b
salima <sahisalima77@gmail.com>
2026-07-31 13:51:16 +0200

    travail sur route product

---
+++ app/api/products/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from "next/server";
+
+import { connectDB } from "@/lib/mongodb";
+import Product from "@/models/Product";
+import { productSchema } from "@/schemas/product.schema";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const validation = productSchema.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(
+        {
+          errors: validation.error.flatten(),
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connectDB();
+
+    const existingProduct = await Product.findOne({
+      sku: body.sku,
+    });
+
+    if (existingProduct) {
+      return NextResponse.json(
+        {
+          message: "Ce SKU existe déjà",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    const product = await Product.create({
+      name: body.name,
+      sku: body.sku,
+      description: body.description,
+      category: body.category,
+      price: body.price,
+      quantity: body.quantity,
+    });
+
+    return NextResponse.json(
+      {
+        message: "Produit créé avec succès",
+        product,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        message: "Erreur serveur",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
